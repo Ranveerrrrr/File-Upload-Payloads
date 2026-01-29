05b46c833350b834f6f370503b4d68f80f0116e4
Ranveerrrrr <ranveer121210@gmail.com>
2026-01-29 14:45:00 +0530

    Add file upload payloads

---
+++ Shell/html.php
@@ -0,0 +1,13 @@
+<?php
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Page</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <p>This whole page is rendered from PHP echo.</p>
+</body>
+</html>
+';
